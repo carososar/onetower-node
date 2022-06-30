e4b7a497f2b8739c12b49657643269cb20330ba1
carolina sosa <carososar@gmail.com>
2022-06-30 08:32:43 -0300

    adaptacion pagina a node

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var nosotrosRouter = require('./routes/nosotros'); //routes/nosotros.js
+var proyectosRouter = require('./routes/proyectos');
+var noticiasRouter = require('./routes/noticias');
+var contactoRouter = require('./routes/contacto');
 
 var app = express();
 
@@ -20,7 +23,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/nosotros', nosotrosRouter);
+app.use('/proyectos', proyectosRouter);
+app.use('/noticias', noticiasRouter);
+app.use('/contacto', contactoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
